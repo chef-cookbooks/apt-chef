a51ed49a5b9a7078c85270ff23079fc47ef2661e
Tim Smith <tsmith84@gmail.com>
2015-09-18 09:44:04 -0700

    Add additional metadata fields

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,12 @@
 name 'apt-chef'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'apache 2.0'
+license 'Apache 2.0'
 description 'Configures apt repository for Chef Software, Inc. products'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 depends 'apt'
+supports 'ubuntu'
+
+source_url 'https://github.com/chef-cookbooks/apt-chef' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/apt-chef/issues' if respond_to?(:issues_url)
