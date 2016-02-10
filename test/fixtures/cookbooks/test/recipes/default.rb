e5bf17a39e00f59c421b0fe354a66af56664d370
Scott Hain <shain@chef.io>
2016-02-10 15:38:11 -0800

    Update tests to include ubuntu 10 and allow for behavior

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,1 +1,4 @@
-package 'chef-ha' # should be a small package for fast install :)
+package 'chef-ha' do # should be a small package for fast install :)
+  # we're all sad about this, but hey
+  options '--force-yes' if platform?('ubuntu') && node['platform_version'] == '10.04'
+end
