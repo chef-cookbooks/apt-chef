6f5584c4645e4d0902d19f7f42300bbb66e37a7c
Tim Smith <tsmith@chef.io>
2017-04-17 15:54:53 -0700

    Update the test recipe to look more like a wrapper cookbook
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,3 +1,5 @@
+include_recipe 'apt-chef::default'
+
 package 'chef-ha' do # should be a small package for fast install :)
   # we're all sad about this, but hey
   options '--force-yes' if platform?('ubuntu') && node['platform_version'] == '10.04'
