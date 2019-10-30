0078e958fd73f04a93610154af62002deff348b4
Tim Smith <tsmith84@gmail.com>
2019-10-30 13:55:39 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Configures apt repository for Chef Software, Inc. products'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.0'
 
 supports 'ubuntu'
