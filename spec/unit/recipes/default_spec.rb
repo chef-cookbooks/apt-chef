f1343419466300591b50bf5d96644108572469ec
Tim Smith <tsmith84@gmail.com>
2020-01-04 21:18:01 -0800

    Fix failing specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,32 +1,11 @@
 require 'spec_helper'
 
 describe 'apt-chef::default' do
-  context 'cookbook attributes are set, on an unspecified platform' do
-    let(:chef_run) do
-      ChefSpec::ServerRunner.new do |node|
-        node.normal['apt-chef'].tap do |apt|
-          apt['repo_name'] = 'chef-nightly'
-          apt['uri'] = 'https://example.com/chef/nightly/ubuntu'
-          apt['gpg'] = 'https://example.com/package-public.key'
-        end
-      end.converge(described_recipe)
-    end
-
-    it 'renders the apt repository from attributes' do
-      expect(chef_run).to add_apt_repository('chef-nightly').with(
-        repo_name: 'chef-nightly',
-        uri: 'https://example.com/chef/nightly/ubuntu',
-        key: 'https://example.com/package-public.key',
-        distribution: 'lucid'
-      )
-    end
-  end
-
-  context 'ubuntu-14.04' do
+  context 'ubuntu-18.04' do
     let(:chef_run) do
       ChefSpec::ServerRunner.new(
         platform: 'ubuntu',
-        version: '14.04'
+        version: '18.04'
       ).converge(described_recipe)
     end
 
@@ -34,8 +13,8 @@ describe 'apt-chef::default' do
       expect(chef_run).to add_apt_repository('chef-stable').with(
         repo_name: 'chef-stable',
         uri: 'https://packages.chef.io/stable-apt',
-        key: 'https://downloads.chef.io/packages-chef-io-public.key',
-        distribution: 'trusty'
+        key: ['https://downloads.chef.io/packages-chef-io-public.key'],
+        distribution: 'lucid'
       )
     end
   end
